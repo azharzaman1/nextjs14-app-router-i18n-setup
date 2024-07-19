2e64ca9c3631396500d1ec0b54b881ae8908fa34
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2024-07-20 02:51:13 +0500

    improvements

--- src/components/some-client-component.tsx
+++ src/components/some-client-component.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 const SomeClientComponent = () => {
   const { t } = useTranslation();
-  return <div>{t("cc.heading")}</div>;
+  return <div className="text-center">{t("cc.heading")}</div>;
 };
 
 export default SomeClientComponent;
